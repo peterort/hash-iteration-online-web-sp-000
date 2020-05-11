faabcd30abfd5495dadf45b12de772ac2f81e744
Peter Ort <peter@peterort.com>
2020-05-11 19:37:46 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -17,15 +17,3 @@ end
 winner
 end
 
-
-birthday_kids = {
-  "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27
-}
-
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
